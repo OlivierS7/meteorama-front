c7485846d1bd4a36830d10e8f9f2a89a2bf81e5e
OlivierS7 <olivier.simonneau@viacesi.fr>
2024-03-16 21:50:21 +0100

    Remove useless import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
