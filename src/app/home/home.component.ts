805e612e5eca7c25fa084bb613c545872921e5cc
OlivierS7 <olivier.simonneau@viacesi.fr>
2024-03-16 15:29:23 +0100

    Link to the Geolocation API

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -48,9 +48,26 @@ export class HomeComponent {
   }
 
   searchWeather(localisation: string) {
-    console.log(localisation);
-    
-    return localisation
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const localisation = { latitude, longitude };
+          this.weatherService.getWeatherWithLatitudeAndLongitude(latitude.toString(), longitude.toString()).subscribe((data) => {
+            this.weather = data
+            console.log(this.weather);
+          })
+        },
+        error => {
+          console.error('Erreur lors de la récupération de la position :', error);
+          // Gérer les erreurs éventuelles ici
+        }
+      );
+    } else {
+      console.error('La géolocalisation n\'est pas prise en charge par ce navigateur.');
+      // Gérer le cas où la géolocalisation n'est pas prise en charge
+    }
   }
 
   onCitySelected() {
